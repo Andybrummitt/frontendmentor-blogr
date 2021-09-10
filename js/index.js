6429a43d5f56b2ec9a744ed4f0aeab8206b95441
Andy <andybrummittps4@hotmail.com>
2021-09-10 19:32:49 +0100

    finished styling mobile menu, currently styling desktop version

--- js/index.js
+++ js/index.js
@@ -3,15 +3,18 @@ console.log('woop')
 //  Navbar Functionality
 
 const menuBtn = document.querySelector('.hb-menu-btn');
+const navMenu = document.querySelector('.nav-menu');
 let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
     if(!menuOpen){
         menuBtn.classList.add('open');
+        navMenu.classList.remove('hide');
         menuOpen = true;
     }
     else {
         menuBtn.classList.remove('open');
+        navMenu.classList.add('hide');
         menuOpen = false;
     }
 })
